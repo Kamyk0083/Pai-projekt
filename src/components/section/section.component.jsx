0546017ca7464802733e35a17e1f61ae47258922
Kamyk0083 <kaminski.ksawery00@gmail.com>
2023-11-25 17:56:10 +0100

    Fix
    fixed that you couldnt have background picture because of the background color

--- src/components/section/section.component.jsx
+++ src/components/section/section.component.jsx
@@ -1,3 +1,5 @@
+// section.jsx
+
 import "./section.scss";
 import { useContext } from "react";
 import { AppContext } from "../../context/app.context";
@@ -10,6 +12,7 @@ const Section = () => {
     sectionTextColor,
     sectionBackgroundColor,
   } = useContext(AppContext);
+
   return (
     <div className="section-container">
       <div
@@ -18,7 +21,7 @@ const Section = () => {
           backgroundImage: `url(${sectionImg[1]})`,
           order: sectionOrder[1],
           color: sectionTextColor[1],
-          background: sectionBackgroundColor[1],
+          backgroundColor: sectionBackgroundColor[1],
         }}
       >
         {sectionText[1]}
@@ -29,7 +32,7 @@ const Section = () => {
           backgroundImage: `url(${sectionImg[2]})`,
           order: sectionOrder[2],
           color: sectionTextColor[2],
-          background: sectionBackgroundColor[2],
+          backgroundColor: sectionBackgroundColor[2],
         }}
       >
         {sectionText[2]}
@@ -40,7 +43,7 @@ const Section = () => {
           backgroundImage: `url(${sectionImg[3]})`,
           order: sectionOrder[3],
           color: sectionTextColor[3],
-          background: sectionBackgroundColor[3],
+          backgroundColor: sectionBackgroundColor[3],
         }}
       >
         {sectionText[3]}
