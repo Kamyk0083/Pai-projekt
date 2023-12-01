24357c4e1c5aa0e4388fefb3fcab36573749fbae
Kamyk0083 <kaminski.ksawery00@gmail.com>
2023-12-01 13:47:02 +0100

    update last i think :D

--- src/components/section/section.component.jsx
+++ src/components/section/section.component.jsx
@@ -36,26 +36,26 @@ const Section = () => {
           <div className="boxOne" style={{ background: boxColor[1] }}>
             <img src={boxImg[1]} alt="F-18" />
             <h2 className="title">{boxTitle[1]}</h2>
-            <p>Top speed: </p>
-            <p>Price: </p>
-            <p>Year of first production: </p>
-            <p>Product made in:</p>
+            <p>Top speed: 1 915 km/h</p>
+            <p>Price: $71.3M</p>
+            <p>Year of first production: 1974</p>
+            <p>Product made in: US</p>
           </div>
           <div className="boxTwo" style={{ background: boxColor[2] }}>
             <img src={boxImg[2]} alt="F-35B" />
             <h2 className="title">{boxTitle[2]}</h2>
-            <p>Top speed: </p>
-            <p>Price: </p>
-            <p>Year of first production:</p>
-            <p>Product made in:</p>
+            <p>Top speed: 1 976 km/h</p>
+            <p>Price: $177M</p>
+            <p>Year of first production: 2015</p>
+            <p>Product made in: US</p>
           </div>
           <div className="boxThree" style={{ background: boxColor[3] }}>
-            <img src={boxImg[3]} alt="Mi 2" />
+            <img src={boxImg[3]} alt="AH-64 Apache" />
             <h2 className="title">{boxTitle[3]}</h2>
-            <p>Top speed: </p>
-            <p>Price: </p>
-            <p>Year of first production:</p>
-            <p>Product made in: </p>
+            <p>Top speed: 293 km/h</p>
+            <p>Price: $52M</p>
+            <p>Year of first production: 1984</p>
+            <p>Product made in: US</p>
           </div>
         </div>
       </div>
